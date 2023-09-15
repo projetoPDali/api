cd46045816bf3c352451b4c8c2e37c5f01c68405
Igor Fonseca <fonsecaigr@gmail.com>
2023-09-15 16:27:16 -0300

    Inserts pgadmin
    criação do teste de inserts

---
+++ inserts.sql
@@ -0,0 +1,20 @@
+insert into bike (id, address, speedkit, frame, rim, hourlyvalue, dayvalue) values
+(1000, 'Rua AAA, 27', 24, 20, 24, 2.50, 20.00),
+(1001, 'Rua BBB, 87', 26, 24, 27, 3.00, 25.00),
+(1002, 'Rua CCC, 92', 26, 27, 29, 4.00, 30.00)
+
+insert into user (id, mail, alias, phone) values
+(1000, 'teste1@mail.com', 'teste1', '(12)99876-5432'),
+(1001, 'teste2@mail.com', 'teste2', '(12)99876-5433'),
+(1002, 'teste3@mail.com', 'teste3', '(12)99876-5434'),
+(1003, 'teste4@mail.com', 'teste4', '(12)99876-5435'),
+
+insert into material (id, name) values
+(1000, 'ferro'),
+(1001, 'aluminio'),
+(1002, 'carbono')
+
+insert into marca (id, name) values
+(1000, 'BMX'),
+(1001, 'CALOI'),
+(1002, 'MONARC')
