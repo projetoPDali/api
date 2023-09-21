413c2d25eccb11019e0b14b211dd8392f61dacd1
Fatec <jcr@fatec.sp.gov.br>
2023-09-20 22:07:39 -0300

    Brand com marcas inseridas

--- src/entities/Brand.ts
+++ src/entities/Brand.ts
@@ -2,9 +2,30 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: "brands" })
 export class Brand {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ nullable: false, length: 50, unique:true })
-    name: string
+  @Column({ nullable: false, length: 50, unique: true })
+  name: string;
+
+  public static enum = {
+    CALOI: "caloi",
+    MONARK: "monark",
+    OGGI: "oggi",
+    SENSE: "sense",
+    SOULS_CYCLES: "souls_cycles",
+    AUDAX: "audax",
+    SENSE_CARBON: "sense_carbon",
+    VZAN: "vzan",
+    MERIDA: "merida",
+    SPECIALIZED: "specialized",
+    CANNONDALE: "cannondale",
+    TREK: "trek",
+    SCOTT: "scott",
+    GIANT: "giant",
+    ORBEA: "orbea",
+    GT: "gt",
+    SANTA_CRUZ: "santa_cruz",
+    OTHER: "outro",
+  };
 }
