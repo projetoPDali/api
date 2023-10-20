23e7ba44232ea000029025dca929b5ed4942b424
Beatriz R. Montanini <montanini249@gmail.com>
2023-10-19 23:25:53 -0300

    endereço cadastrado junto ao user

--- src/controllers/BikeController.ts
+++ src/controllers/BikeController.ts
@@ -10,66 +10,6 @@ import { Gender } from "../entities/Gender";
 class BikeController {
   public async create(req: Request, res: Response): Promise<Response> {
     const {
-      iduser,
-      idmaterial,
-      idbrand,
-      size,
-      idgender,
-      gear,
-      rim,
-      suspension,
-      description,
-      hourlyvalue,
-      dailyvalue,
-      idaddress,
-    } = req.body;
-
-    //obtém o usuário na tabela users
-    const user = await AppDataSource.manager.findOneBy(User, { id: iduser });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ error: "Usuário desconhecido", props: "user" });
-    }
-
-    //obtém a marca na tabela brands
-    const brand = await AppDataSource.manager.findOneBy(Brand, { id: idbrand });
-    if (!brand) {
-      return res
-        .status(400)
-        .json({ error: "Marca desconhecida", props: "brand" });
-    }
-
-    //obtém o genero na tabela Gender
-    const gender = await AppDataSource.manager.findOneBy(Gender, { id: idgender });
-    if (!gender) {
-      return res
-        .status(400)
-        .json({ error: "Genero não cadastrado", props: "gender" });
-    }
-
-    //obtém o endereço na tabela address
-    const address = await AppDataSource.manager.findOneBy(Address, { id: idaddress });
-    if (!address) {
-      return res
-        .status(400)
-        .json({ error: "Endereço não encontrado", props: "address" });
-    }
-
-    //obtém a material na tabela material
-    const material = await AppDataSource.manager.findOneBy(Material, {
-      id: idmaterial,
-    });
-    if (!material) {
-      return res
-        .status(400)
-        .json({ error: "Material desconhecido", props: "Material" });
-    }
-
-    const bike = await AppDataSource.manager.save(Bike, {
-      user,
-      brand,
-      material,
       size,
       gender,
       gear,
@@ -78,10 +18,76 @@ class BikeController {
       description,
       hourlyvalue,
       dailyvalue,
+      user,
+      brand,
+      material,
       address,
+    } = req.body;
+  
+    // Verifique se o endereço idêntico já existe no banco de dados
+    const existingAddress = await AppDataSource.manager.findOneBy(Address, address);
+  
+    let addressEntity;
+    if (existingAddress) {
+      addressEntity = existingAddress;
+    } else {
+      addressEntity = await AppDataSource.manager.save(Address, address);
+    }
+  
+    // Verifique se o gênero já existe no banco de dados
+    const existingGender = await AppDataSource.manager.findOneBy(Gender, { name: gender.name });
+  
+    let genderEntity;
+    if (existingGender) {
+      genderEntity = existingGender;
+    } else {
+      genderEntity = await AppDataSource.manager.save(Gender, gender);
+    }
+  
+    // Verifique se a marca já existe no banco de dados
+    const existingBrand = await AppDataSource.manager.findOneBy(Brand, { name: brand.name });
+  
+    let brandEntity;
+    if (existingBrand) {
+      brandEntity = existingBrand;
+    } else {
+      brandEntity = await AppDataSource.manager.save(Brand, brand);
+    }
+  
+    // Verifique se o material já existe no banco de dados
+    const existingMaterial = await AppDataSource.manager.findOneBy(Material, { name: material.name });
+  
+    let materialEntity;
+    if (existingMaterial) {
+      materialEntity = existingMaterial;
+    } else {
+      materialEntity = await AppDataSource.manager.save(Material, material);
+    }
+  
+    // Obtenha o usuário na tabela users
+    const userEntity = await AppDataSource.manager.findOneBy(User, { id: user });
+    if (!userEntity) {
+      return res.status(400).json({ error: "Usuário desconhecido", props: "user" });
+    }
+  
+    const bike = await AppDataSource.manager.save(Bike, {
+      size,
+      gender: genderEntity,
+      gear,
+      rim,
+      suspension,
+      description,
+      hourlyvalue,
+      dailyvalue,
+      user: userEntity,
+      brand: brandEntity,
+      material: materialEntity,
+      address: addressEntity,
     });
+  
     return res.json(bike);
   }
+  
 
   public async update(req: Request, res: Response): Promise<Response> {
     const {
--- src/entities/Address.ts
+++ src/entities/Address.ts
@@ -12,11 +12,16 @@ export class Address {
   @Column({ nullable: false, length: 50 })
   city: string;
 
+  @Column({ nullable: true, length: 50 })
+  neighborhood: string;
+
   @Column({ nullable: false, length: 50 })
   state: string;
 
   @Column({ nullable: false, })
   cep: number;
 
+  @Column({ nullable: true, })
+  number: number;
   // Outros campos de endereço, se necessário
 }
